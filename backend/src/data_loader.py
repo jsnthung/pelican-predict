9d4cc1afd9d2ca201267af85e80c0d3c2955b0e4
raymondkings <raymond.setia@tum.de>
2025-04-26 12:16:35 +0200

    load financial news

--- backend/src/data_loader.py
+++ backend/src/data_loader.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import os
 from typing import Dict,List
 
 import yfinance as yf
@@ -6,6 +7,12 @@ import pandas as pd
 import numpy as np
 from alpaca.data.historical.news import NewsClient
 from alpaca.data.requests import NewsRequest
+from pathlib import Path
+from dotenv import load_dotenv
+
+# load env file since it is one dir above
+env_path = Path(__file__).parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
 
 YEARS_OF_FINANCIALS = 4
 NEWS_LIMIT = 15
@@ -87,3 +94,39 @@ def convert_fundamentals_to_JSON(raw_data: dict) -> dict:
 
     return {k: clean_value(v) for k, v in raw_data.items()}
 
+NEWS_LOOKBACK_DAYS = 30
+NEWS_LIMIT_PER_TICKER = 15
+
+def fetch_news(ticker:str) -> list[dict]:
+    alpaca_api_key = os.getenv("ALPACA_API_KEY_ID")
+    alpaca_api_secret = os.getenv("ALPACA_API_SECRET_KEY")
+
+    if not alpaca_api_key or not alpaca_api_secret:
+        raise RuntimeError("Alpaca API Key missing")
+
+    client = NewsClient(api_key=alpaca_api_key, secret_key=alpaca_api_secret)
+    start_date = (pd.Timestamp.today() - pd.Timedelta(days=NEWS_LOOKBACK_DAYS)).strftime("%Y-%m-%d")
+
+    req = NewsRequest(
+        symbols=ticker,
+        start=start_date,
+        limit=NEWS_LIMIT_PER_TICKER
+    )
+
+    try:
+        news_df = client.get_news(req).df
+    except Exception as e:
+        print(f"failed to fetch news for {ticker}: {e}")
+        return []
+
+    # Only keep the important fields
+    articles = [
+        {
+            "headline": row["headline"],
+            "summary": row["summary"],
+            "url": row["url"]
+        }
+        for _, row in news_df.iterrows()
+    ]
+
+    return articles
